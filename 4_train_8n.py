5145741229b816e38ce0dcf3913f0e911f35a4fe
T.kawabata <52056772+momorara@users.noreply.github.com>
2025-11-19 16:19:22 +0900

    Update 4_train_8n.py

--- 4_train_8n.py
+++ 4_train_8n.py
@@ -3,7 +3,7 @@
 license
 GNU Affero General Public License v3（AGPL v3）
 
-yolo detect train data=data.yaml model=yolov8n.pt epochs=3 imgsz=320
+yolo detect train data=data.yaml model=yolov8n.pt epochs=2 imgsz=128
 として処理した結果を使う
 
 対象画像はdata.yamlで指定
@@ -11,4 +11,4 @@ yolo detect train data=data.yaml model=yolov8n.pt epochs=3 imgsz=320
 from ultralytics import YOLO
 
 model = YOLO("yolov8n.pt")
-results = model.train(data="data.yaml", epochs=40, imgsz=128)
+results = model.train(data="data.yaml", epochs=2, imgsz=128)
